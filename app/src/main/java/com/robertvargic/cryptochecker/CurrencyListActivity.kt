a82ae78ad89d0e6e20a129147e74ec9bd8d91b76
Robert Vargic <robert.vargic@retrofitme.com>
2018-04-13 12:50:57 +0200

    Build fixes.

--- app/src/main/java/com/robertvargic/cryptochecker/CurrencyListActivity.kt
+++ app/src/main/java/com/robertvargic/cryptochecker/CurrencyListActivity.kt
@@ -2,6 +2,7 @@ package com.robertvargic.cryptochekerjava
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.robertvargic.cryptochecker.R
 
 class CurrencyListActivity : AppCompatActivity() {
 
