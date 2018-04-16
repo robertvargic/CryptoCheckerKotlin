5f4ee4ab1b4938b4defc8193f9e650ee1c15d48f
Robert Vargic <robert.vargic@gmail.com>
2018-04-16 15:40:55 +0200

    Updated gradle, added Retrofit util.

---
+++ app/src/main/java/com/robertvargic/cryptochecker/networking/RetrofitUtil.kt
@@ -0,0 +1,36 @@
+package com.robertvargic.cryptochecker.networking
+
+import android.content.Context
+import com.google.gson.*
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
+
+class RetrofitUtil {
+
+    private val DATE_JSON_SERIALIZER = JsonSerializer<Date> { src, typeOfSrc, context -> if (src == null) null else JsonPrimitive(src.time) }
+    private val DATE_JSON_DESERIALIZER = JsonDeserializer<Date> { json, typeOfT, context -> if (json == null) null else Date(json.asLong) }
+
+    fun createRetrofitForUrl(context: Context): Retrofit {
+        return createRetrofit(context)
+    }
+
+    private fun createRetrofit(context: Context): Retrofit {
+        return Retrofit.Builder()
+                .baseUrl("https://api.coinmarketcap.com/")
+                .addConverterFactory(GsonConverterFactory.create(getGson()))
+                .build()
+    }
+
+    private fun getGson(): Gson {
+        return getCommonBuilder()
+                .create()
+    }
+
+    private fun getCommonBuilder(): GsonBuilder {
+        return GsonBuilder()
+                .registerTypeAdapter(Date::class.java, DATE_JSON_SERIALIZER)
+                .registerTypeAdapter(Date::class.java, DATE_JSON_DESERIALIZER)
+                .excludeFieldsWithoutExposeAnnotation()
+    }
+}
